c739ef3b34de9dd348e7595d8d0cc32e4b300135
NikitaNimse <nimsenikita710>
2024-08-04 13:57:37 +0530

    home view added

---
+++ src/views/Home/Home.js
@@ -0,0 +1,59 @@
+import'./Home.css'
+import React, { useEffect, useState } from 'react'
+import axios from 'axios';
+import Watchcard from "../../components/WatchCard/WatchCard.js"
+import { Link } from 'react-router-dom';
+import Imgadd from './Add.png'
+
+function Home() {
+
+
+    const [watches,setwatches]= useState([])
+
+
+    const Loadwatches =  async ()=>{
+        const response = await axios.get(`${process.env.React_APP_API_URL}/watches`)
+        setwatches(response.data.data) 
+      }
+
+    useEffect(()=>{
+      Loadwatches()  
+    },[])
+
+  return (
+    <div>
+        <h1>watches</h1>
+     {
+        watches.map((watch,i)=>{
+
+            const {_id,
+                name,
+               company,
+                image,
+                price,
+                description} = watch
+
+        return (<Watchcard
+          key={i}
+          _id={_id}
+          name={name}
+          company={company}
+          image={image}
+          price={price}
+          description={description}
+         Loadwatches ={Loadwatches }
+         />
+        )
+        })
+           
+     }
+     <Link to="/add">
+     <img src={Imgadd} className='add-btn' alt="add"/>
+     </Link>
+
+    </div>
+  )
+}
+
+
+export default Home
